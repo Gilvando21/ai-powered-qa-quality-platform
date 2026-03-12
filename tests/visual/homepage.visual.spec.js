67a7403f9bd36e72c52a410c3cd46b5f294ee087
GilvandoSenaMatos <gilvando0321@gmail.com>
2026-03-12 15:12:44 -0300

    Improve visual regression test tolerance

--- tests/visual/homepage.visual.spec.js
+++ tests/visual/homepage.visual.spec.js
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test'
 
 test('homepage visual regression', async ({ page }) => {
+
   await page.goto('https://example.com')
-  expect(await page.screenshot()).toMatchSnapshot('homepage.png')
+
+  expect(await page.screenshot()).toMatchSnapshot('homepage.png', {
+    maxDiffPixelRatio: 0.05
+  })
+
 })
